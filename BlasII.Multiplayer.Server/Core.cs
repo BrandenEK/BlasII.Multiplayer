8830fb4086c0761c33ae9d99b72d1f6cc54e705b
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-08-23 17:18:51 -0400

    core: add server project

---
+++ BlasII.Multiplayer.Server/Core.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BlasII.Multiplayer.Server;
+
+internal class Core
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        while (true)
+        {
+            Console.ReadKey(true);
+        }
+    }
+}
