d94d7dbf2909e597574f9afb5c2d885c3974474d
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-08-23 22:33:48 -0400

    server: add playerinfo

---
+++ BlasII.Multiplayer.Server/Models/PlayerInfo.cs
@@ -0,0 +1,19 @@
+﻿
+namespace BlasII.Multiplayer.Server.Models;
+
+public class PlayerInfo(string name)
+{
+    public string Name { get; } = name;
+
+    public float PositionX { get; set; } = 0;
+    public float PositionY { get; set; } = 0;
+
+    public int AnimationState { get; set; } = 0;
+    public float AnimationLength { get; set; } = 0;
+
+    public bool FacingDirection { get; set; } = true;
+
+    public string ArmorName { get; set; } = string.Empty;
+    public string WeaponName { get; set; } = string.Empty;
+    public string WeaponfxName { get; set; } = string.Empty;
+}
