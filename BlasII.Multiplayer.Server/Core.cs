b6d27b385ced0cffe59a3697494ac0fd8d162f19
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-08-23 22:04:54 -0400

    core: add server project (#5)
    * core: add server project
    
    * server: setup logging
    
    * server: upgrade logging framework

---
+++ BlasII.Multiplayer.Server/Core.cs
@@ -0,0 +1,23 @@
+﻿using Basalt.Framework.Logging;
+using Basalt.Framework.Logging.Standard;
+using System;
+using System.Threading;
+
+namespace BlasII.Multiplayer.Server;
+
+internal class Core
+{
+    static void Main(string[] args)
+    {
+        Logger.AddLoggers(new ConsoleLogger(TITLE), new FileLogger(Environment.CurrentDirectory));
+
+        Logger.Warn("This is a test warning");
+
+        while (true)
+        {
+            Thread.Sleep(100);
+        }
+    }
+
+    private const string TITLE = "Blasphemous 2 Multiplayer Server";
+}
