d5a829a28630e32e23fd67727316cd492983997c
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-09-22 09:43:20 -0400

    client: basic connection process

--- BlasII.Multiplayer.Client/Multiplayer.cs
+++ BlasII.Multiplayer.Client/Multiplayer.cs
@@ -1,4 +1,7 @@
-﻿using BlasII.ModdingAPI;
+﻿using Basalt.Framework.Networking;
+using Basalt.Framework.Networking.Client;
+using Basalt.Framework.Networking.Serializers;
+using BlasII.ModdingAPI;
 using BlasII.ModdingAPI.Helpers;
 using BlasII.Multiplayer.Client.Storages;
 using Il2CppTGK.Game;
@@ -7,6 +10,8 @@ namespace BlasII.Multiplayer.Client;
 
 public class Multiplayer : BlasIIMod
 {
+    public NetworkClient NetworkClient { get; }
+
     public CompanionHandler CompanionHandler { get; }
     public PlayerHandler PlayerHandler { get; }
 
@@ -14,6 +19,11 @@ public class Multiplayer : BlasIIMod
 
     internal Multiplayer() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION)
     {
+        NetworkClient = new NetworkClient(new ClassicSerializer());
+        NetworkClient.OnClientConnected += TEMP_OnConnect;
+        NetworkClient.OnClientDisconnected += TEMP_OnDisconnect;
+        NetworkClient.OnPacketReceived += TEMP_OnReceive;
+
         CompanionHandler = new CompanionHandler();
         PlayerHandler = new PlayerHandler();
 
@@ -32,6 +42,14 @@ public class Multiplayer : BlasIIMod
 
         CompanionHandler.OnUpdate();
         PlayerHandler.OnUpdate();
+
+        if (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.Equals))
+        {
+            // Connect
+            NetworkConnect();
+        }
+
+        NetworkUpdate();
     }
 
     protected override void OnSceneLoaded(string sceneName)
@@ -47,5 +65,53 @@ public class Multiplayer : BlasIIMod
     {
         CompanionHandler.OnLeaveScene();
         PlayerHandler.OnLeaveScene();
+    }
+
+    private void TEMP_OnConnect(string ip)
+    {
+        ModLog.Warn("Now connected");
+    }
+
+    private void TEMP_OnDisconnect(string ip)
+    {
+        ModLog.Warn("Now disconnected");
+    }
+
+    private void TEMP_OnReceive(BasePacket packet)
+    {
+        ModLog.Error("Received a packet");
+    }
+
+    private void NetworkConnect()
+    {
+        ModLog.Info($"Trying to connect to localhost:33002");
+
+        try
+        {
+            NetworkClient.Connect("localhost", 33002);
+            ModLog.Info($"Client connected to localhost:33002");
+        }
+        catch (System.Exception ex)
+        {
+            ModLog.Error($"Encountered an error when attempting to connect - {ex}");
+            return;
+        }
+    }
+
+    private void NetworkUpdate()
+    {
+        if (!NetworkClient.IsActive)
+            return;
+
+        try
+        {
+            NetworkClient.Receive();
+        }
+        catch (NetworkException ex)
+        {
+            ModLog.Error($"Encountered an error when receiving data - {ex}");
+        }
+
+        NetworkClient.Update();
     }
 }
