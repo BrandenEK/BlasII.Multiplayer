130745969d488b5f878ec05ee25c8321cfa8905b
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-09-22 17:05:30 -0400

    client: add nametags (#10)
    * core: add nametaghandler
    
    * client: implement nametag handler
    
    * client: only do nametags if connected
    
    * client: remove extra log statment
    
    * client: properly update nametag positions

---
+++ BlasII.Multiplayer.Client/Nametags/Nametag.cs
@@ -0,0 +1,43 @@
+﻿using BlasII.Framework.UI;
+using Il2CppTGK.Game.Components.UI;
+using UnityEngine;
+
+namespace BlasII.Multiplayer.Client.Nametags;
+
+public class Nametag
+{
+    private readonly UIPixelTextWithShadow _text;
+
+    public Vector3 Position { get; set; }
+
+    public Nametag(string name)
+    {
+        _text = UIModder.Create(new RectCreationOptions()
+        {
+            Name = name + "_tag",
+            Parent = UIModder.Parents.GameLogic,
+            Size = new Vector2(100, 50),
+        }).AddText(new TextCreationOptions()
+        {
+            Alignment = Il2CppTMPro.TextAlignmentOptions.Bottom,
+            Color = new Color32(0xAB, 0x9A, 0x3F, 0xFF),
+            Contents = name,
+            FontSize = 48,
+        }).AddShadow();
+    }
+
+    public void UpdateProperties(Camera camera)
+    {
+        Vector3 viewPos = camera.WorldToViewportPoint(Position + Vector3.up * NAMETAG_OFFSET);
+        _text.shadowText.rectTransform.anchorMin = viewPos;
+        _text.shadowText.rectTransform.anchorMax = viewPos;
+        _text.shadowText.rectTransform.anchoredPosition = Vector2.zero;
+    }
+
+    public void Destroy()
+    {
+        Object.Destroy(_text.gameObject);
+    }
+
+    private const float NAMETAG_OFFSET = 2.9f;
+}
---
+++ BlasII.Multiplayer.Client/Nametags/NametagHandler.cs
@@ -0,0 +1,113 @@
+﻿using Basalt.Framework.Networking;
+using Basalt.Framework.Networking.Client;
+using BlasII.ModdingAPI;
+using BlasII.ModdingAPI.Utils;
+using BlasII.Multiplayer.Core.Packets;
+using Il2CppTGK.Game;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BlasII.Multiplayer.Client.Nametags;
+
+public class NametagHandler
+{
+    private readonly NetworkClient _client;
+    private readonly Dictionary<string, Nametag> _nametags;
+    private readonly ObjectCache<Camera> _camCache;
+
+    public NametagHandler(NetworkClient client)
+    {
+        _client = client;
+        _nametags = [];
+        _camCache = new ObjectCache<Camera>(() => Object.FindObjectsOfType<Camera>().First(x => x.name == "Main Camera"));
+
+        client.OnPacketReceived += OnPacketReceived;
+        client.OnClientDisconnected += OnClientDisconnected;
+    }
+
+    public void OnEnterScene()
+    {
+        RemoveAllNametags();
+    }
+
+    public void OnLeaveScene()
+    {
+        RemoveAllNametags();
+    }
+
+    public void OnUpdate()
+    {
+        // TODO: remove nametag when someone leaves room
+
+        if (!_client.IsActive)
+            return;
+
+        OnReceivePosition(Multiplayer.PlayerName, CoreCache.PlayerSpawn.PlayerInstance.transform.position);
+
+        foreach (var nametag in _nametags.Values)
+        {
+            nametag.UpdateProperties(_camCache.Value);
+        }
+    }
+
+    private void OnClientDisconnected(string ip)
+    {
+        RemoveAllNametags();
+    }
+
+    private void OnPacketReceived(BasePacket packet)
+    {
+        if (packet is PositionPacket position)
+            OnReceivePosition(position.Name, new Vector2(position.X, position.Y));
+
+        // Should I ensure the nametag already exists from the scenepacket or just create them if they dont exist yet ??
+    }
+
+    private void OnReceivePosition(string name, Vector2 position)
+    {
+        if (!_nametags.TryGetValue(name, out Nametag nametag))
+        {
+            nametag = AddNametag(name);
+            //ModLog.Error($"Received position from {name} who does not exist");
+            //return;
+        }
+
+        nametag.Position = position;
+    }
+
+    private Nametag AddNametag(string name)
+    {
+        if (_nametags.TryGetValue(name, out Nametag nametag))
+        {
+            ModLog.Warn($"Failed to add nametag {name} because they already exist");
+            return nametag;
+        }
+
+        ModLog.Info($"Adding nametag {name}");
+
+        nametag = new Nametag(name);
+        _nametags.Add(name, nametag);
+        return nametag;
+    }
+
+    private void RemoveNametag(string name)
+    {
+        if (!_nametags.TryGetValue(name, out Nametag nametag))
+        {
+            ModLog.Warn($"Failed to remove nametag {name} because they don't exist");
+            return;
+        }
+
+        ModLog.Info($"Removing nametag {name}");
+        nametag.Destroy();
+        _nametags.Remove(name);
+    }
+
+    private void RemoveAllNametags()
+    {
+        foreach (var nametag in _nametags.Values)
+            nametag.Destroy();
+        _nametags.Clear();
+    }
+}
