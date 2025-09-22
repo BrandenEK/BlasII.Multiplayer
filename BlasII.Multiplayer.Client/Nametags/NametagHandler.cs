7fa17859a954fe998e667994d4d8f78e985353ad
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-09-22 17:04:49 -0400

    client: properly update nametag positions

--- BlasII.Multiplayer.Client/Nametags/NametagHandler.cs
+++ BlasII.Multiplayer.Client/Nametags/NametagHandler.cs
@@ -1,11 +1,9 @@
 ﻿using Basalt.Framework.Networking;
 using Basalt.Framework.Networking.Client;
-using BlasII.Framework.UI;
 using BlasII.ModdingAPI;
 using BlasII.ModdingAPI.Utils;
 using BlasII.Multiplayer.Core.Packets;
 using Il2CppTGK.Game;
-using Il2CppTGK.Game.Components.UI;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -15,7 +13,7 @@ namespace BlasII.Multiplayer.Client.Nametags;
 public class NametagHandler
 {
     private readonly NetworkClient _client;
-    private readonly Dictionary<string, UIPixelTextWithShadow> _nametags;
+    private readonly Dictionary<string, Nametag> _nametags;
     private readonly ObjectCache<Camera> _camCache;
 
     public NametagHandler(NetworkClient client)
@@ -46,6 +44,11 @@ public class NametagHandler
             return;
 
         OnReceivePosition(Multiplayer.PlayerName, CoreCache.PlayerSpawn.PlayerInstance.transform.position);
+
+        foreach (var nametag in _nametags.Values)
+        {
+            nametag.UpdateProperties(_camCache.Value);
+        }
     }
 
     private void OnClientDisconnected(string ip)
@@ -63,22 +66,19 @@ public class NametagHandler
 
     private void OnReceivePosition(string name, Vector2 position)
     {
-        if (!_nametags.TryGetValue(name, out UIPixelTextWithShadow nametag))
+        if (!_nametags.TryGetValue(name, out Nametag nametag))
         {
             nametag = AddNametag(name);
             //ModLog.Error($"Received position from {name} who does not exist");
             //return;
         }
 
-        Vector3 viewPos = _camCache.Value.WorldToViewportPoint(position + Vector2.up * NAMETAG_OFFSET);
-        nametag.shadowText.rectTransform.anchorMin = viewPos;
-        nametag.shadowText.rectTransform.anchorMax = viewPos;
-        nametag.shadowText.rectTransform.anchoredPosition = Vector2.zero;
+        nametag.Position = position;
     }
 
-    private UIPixelTextWithShadow AddNametag(string name)
+    private Nametag AddNametag(string name)
     {
-        if (_nametags.TryGetValue(name, out UIPixelTextWithShadow nametag))
+        if (_nametags.TryGetValue(name, out Nametag nametag))
         {
             ModLog.Warn($"Failed to add nametag {name} because they already exist");
             return nametag;
@@ -86,42 +86,28 @@ public class NametagHandler
 
         ModLog.Info($"Adding nametag {name}");
 
-        nametag = UIModder.Create(new RectCreationOptions()
-        {
-            Name = name + "_tag",
-            Parent = UIModder.Parents.GameLogic,
-            Size = new Vector2(100, 50),
-        }).AddText(new TextCreationOptions()
-        {
-            Alignment = Il2CppTMPro.TextAlignmentOptions.Bottom,
-            Color = new Color32(0xAB, 0x9A, 0x3F, 0xFF),
-            Contents = name,
-            FontSize = 48,
-        }).AddShadow();
-
+        nametag = new Nametag(name);
         _nametags.Add(name, nametag);
         return nametag;
     }
 
     private void RemoveNametag(string name)
     {
-        if (!_nametags.TryGetValue(name, out UIPixelTextWithShadow nametag))
+        if (!_nametags.TryGetValue(name, out Nametag nametag))
         {
             ModLog.Warn($"Failed to remove nametag {name} because they don't exist");
             return;
         }
 
         ModLog.Info($"Removing nametag {name}");
-        Object.Destroy(nametag.gameObject);
+        nametag.Destroy();
         _nametags.Remove(name);
     }
 
     private void RemoveAllNametags()
     {
         foreach (var nametag in _nametags.Values)
-            Object.Destroy(nametag.gameObject);
+            nametag.Destroy();
         _nametags.Clear();
     }
-
-    private const float NAMETAG_OFFSET = 2.9f;
 }
